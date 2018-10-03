aeb400cc2050c8b771b6d87da22cb995e8f1c607
Iabin Arteaga <iabin@ciencias.unam.mx>
2018-10-02 19:43:31 -0500

    gitnore added

--- src/Main.java
+++ src/Main.java
@@ -18,14 +18,14 @@ public class Main {
                                 +"              where 'n' and 'd' are integers,\n"
                                 +"          the output will be printed on the standard ouput"
                                 +"           If you're using a linux system, you can use "
-                                +"              java -jar MaurerRose.jar n d > file.html" 
+                                +"              java -jar MaurerRose.jar n d > file.html"
                                 +"        if you need more information, please check:\n"
                                 +"          https://en.wikipedia.org/wiki/Maurer_rose \n \n");
             return;
         }//It prints the HTML on the  standard output
         System.out.println("<title>Maurer Rose</title>"
                          +"<head><style> *{font-size:80px; text-align:center;}</style><head>"
-                         +"<h1>Maurer Rose whit parameters n = "+args[0]+"   d = "+args[1]+"</h1>"
+                         +"<h1>Maurer Rose with parameters n = "+args[0]+"   d = "+args[1]+"</h1>"
                          +"<svg width='1500' height='1500'>");
         rosa.imprimeSVG();
         System.out.println("</svg>");
